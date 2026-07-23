2edf3aad96aa857aa7f81060c572f0917a449951
jkjk9481-max <GitHub jkjk9481@kyungmin.ac.kr>
2026-07-23 14:43:19 +0900

    UserController 작성 , 다음 Postman 작동해보기

--- src/main/java/com/carrentall/backend/user/controller/UserController.java
+++ src/main/java/com/carrentall/backend/user/controller/UserController.java
@@ -1,11 +1,24 @@
 package com.carrentall.backend.user.controller;
 
+
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
+
+
+    //  → JwtAuthenticationFilter가 JWT 검사
+    //  → JWT에서 email 추출
+    //  → Authentication의 principal에 email 저장
+    //  → UserController가 authentication.getName()으로 email 확인
+    @GetMapping("/me")
+    public String getMyInfo(Authentication authentication) {
+        return authentication.getName();
+    }
 
 
 }
